5cbdf8f402a4fc80b28cdc67e3def615ada11dc5
Karolina Kuchta <linka_94@interia.eu>
2019-10-17 11:07:46 +0200

    Finished react form

--- components/Contact.js
+++ components/Contact.js
@@ -6,14 +6,17 @@ var Contact = React.createClass({
     render: function() {
       return (
         React.createElement('div', {className: 'contactItem'},
-          React.createElement('i', {
-            className: 'fas fa-id-card-alt'            
-          }),
-          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-            this.props.item.email
-          )
+          React.createElement('i', {className: 'fas fa-id-card-alt'}),
+          React.createElement('div', {className: 'personalData'},
+            React.createElement('div', {className: 'name'}, 
+              React.createElement('p', {className: 'contactLabel'}, this.props.item.firstName),
+              React.createElement('p', {className: 'contactLabel'}, this.props.item.lastName),
+            ),
+              React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+              this.props.item.email
+            )
+          ),
+          React.createElement('i', {className: 'fas fa-info-circle'})
         )
       )
     },
--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,25 +6,37 @@ var ContactForm = React.createClass({
     render: function() {
       return (
         React.createElement('form', {className: 'contactForm'},
-          React.createElement('p', {className: 'nameLabel'}, 'Imię'),
-          React.createElement('input', {
-            type: 'text',
-            placeholder: 'Imię',
-            value: this.props.contact.firstName            
-          }),
-          React.createElement('p', {className: 'nameLabel'}, 'Nazwisko'),
-          React.createElement('input', {
-            type: 'text',
-            placeholder: 'Nazwisko',
-            value: this.props.contact.lastName,
-          }),
-          React.createElement('p', {className: 'formLabel'}, 'Email'),
+          React.createElement('div', {className: 'nameRow'}, 
+            React.createElement('div', {className: 'first form-group'},
+              React.createElement('label', {className: 'nameLabel', for: 'inputFirstName'}, 'Imię'),
+              React.createElement('input', {
+                type: 'text',
+                placeholder: 'Imię',
+                id: 'inputFirstName',
+                className: 'form-control',
+                value: this.props.contact.firstName            
+              })
+            ),
+            React.createElement('div', {className: 'last form-group'},
+              React.createElement('label', {className: 'nameLabel', for: 'inputLastName'}, 'Nazwisko'),
+              React.createElement('input', {
+                type: 'text',
+                placeholder: 'Nazwisko',
+                id: 'inputLastName',
+                className: 'form-control',
+                value: this.props.contact.lastName,
+              })
+            )
+          ),
+          React.createElement('label', {className: 'formLabel', for: 'inputEmail'}, 'Email'),
           React.createElement('input', {
             type: 'email',
             placeholder: 'Email',
+            id: 'inputEmail',
+            className: 'form-control',
             value: this.props.contact.email,
           }),
-          React.createElement('button', {type: 'submit', className: 'submit'}, "Dodaj kontakt")
+          React.createElement('button', {type: 'submit', className: 'submit btn btn-info'}, "Dodaj kontakt")
         )
       )
     },
