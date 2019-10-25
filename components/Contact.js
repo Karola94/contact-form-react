6e0144f108c54943f4be25381b303edfa870cfc9
Karolina Kuchta <linka_94@interia.eu>
2019-10-25 16:36:01 +0200

    Change contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -5,19 +5,41 @@ var Contact = React.createClass({
   
     render: function() {
       return (
-        React.createElement('div', {className: 'contactItem'},
-          React.createElement('i', {className: 'fas fa-id-card-alt'}),
-          React.createElement('div', {className: 'personalData'},
-            React.createElement('div', {className: 'name'}, 
-              React.createElement('p', {className: 'contactLabel'}, this.props.item.firstName),
-              React.createElement('p', {className: 'contactLabel'}, this.props.item.lastName),
-            ),
-              React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-              this.props.item.email
-            )
-          ),
-          React.createElement('i', {className: 'fas fa-info-circle'})
-        )
-      )
-    },
+        <div className={'contactItem'}>
+          <i className={'fas fa-id-card-alt'}></i>
+          <div className={'personalData'}>
+            <div className={'name'}>
+              <p className={'contactLabel'}>
+                {this.props.item.firstName}
+              </p>
+              <p className={'contactLabel'}>
+                {this.props.item.lastName}
+              </p>
+            </div>
+            <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>
+              {this.props.item.email}
+            </a>
+          </div>
+          <i className={'fas fa-info-circle'}></i>    
+        </div>
+      );
+    }
 });
+
+
+
+
+
+// React.createElement('div', {className: 'contactItem'},
+//   React.createElement('i', {className: 'fas fa-id-card-alt'}),
+//   React.createElement('div', {className: 'personalData'},
+//     React.createElement('div', {className: 'name'}, 
+//       React.createElement('p', {className: 'contactLabel'}, this.props.item.firstName),
+//       React.createElement('p', {className: 'contactLabel'}, this.props.item.lastName),
+//     ),
+//       React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+//       this.props.item.email
+//     )
+//   ),
+//   React.createElement('i', {className: 'fas fa-info-circle'})
+// )
